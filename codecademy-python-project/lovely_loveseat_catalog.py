555f3797b59654ec5df4cbe717d9fe054223bdae
Enrico <159620783+Enrico1391@users.noreply.github.com>
2024-02-12 11:01:51 +0100

    Update lovely_loveseat_catalog.py

--- codecademy-python-project/lovely_loveseat_catalog.py
+++ codecademy-python-project/lovely_loveseat_catalog.py
@@ -1,13 +1,16 @@
 # Variables for the first item itemization and price
-lovely_loveseat_description = '''Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.'''
+lovely_loveseat_description = '''Lovely Loveseat. Tufted polyester blend on wood. 
+32 inches high x 40 inches wide x 30 inches deep. Red or white.'''
 lovely_loveseat_price = 254.00
 
 # Variables for the second item itemization and price
-stylish_settee_description = '''Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.'''
+stylish_settee_description = '''Stylish Settee. Faux leather on birch. 
+29.50 inches high x 54.75 inches wide x 28 inches deep. Black.'''
 stylish_settee_price = 180.50
 
 # Variables for the third item itemization and price
-luxurious_lamp_description = '''Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.'''
+luxurious_lamp_description = '''Luxurious Lamp. Glass and iron. 
+36 inches tall. Brown with cream shade.'''
 luxurious_lamp_price = 52.15
 
 # Sales tax rate
