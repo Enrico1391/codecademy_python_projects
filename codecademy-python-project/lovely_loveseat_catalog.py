1230554c652e62f1d23030755488684acf84fee9
Enrico <159620783+Enrico1391@users.noreply.github.com>
2024-02-14 17:54:49 +0100

    Update lovely_loveseat_catalog.py

--- codecademy-python-project/lovely_loveseat_catalog.py
+++ codecademy-python-project/lovely_loveseat_catalog.py
@@ -1,14 +1,17 @@
-# Variables for the first item itemization and price
+# Lovely Loveseat (lovely_loveseat_catalog.py)
+# Keep receipts for the furniture store, lovely loveseats.
+
+# Declare variables for the first item itemization and price
 lovely_loveseat_description = '''Lovely Loveseat. Tufted polyester blend on wood. 
 32 inches high x 40 inches wide x 30 inches deep. Red or white.'''
 lovely_loveseat_price = 254.00
 
-# Variables for the second item itemization and price
+# Declare variables for the second item itemization and price
 stylish_settee_description = '''Stylish Settee. Faux leather on birch. 
 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.'''
 stylish_settee_price = 180.50
 
-# Variables for the third item itemization and price
+# Declare variables for the third item itemization and price
 luxurious_lamp_description = '''Luxurious Lamp. Glass and iron. 
 36 inches tall. Brown with cream shade.'''
 luxurious_lamp_price = 52.15
@@ -16,7 +19,7 @@ luxurious_lamp_price = 52.15
 # Sales tax rate
 sales_tax = .088
 
-# Variables for the first customer expenses
+# Declare variables for the first customer expenses
 customer_one_total = 0
 customer_one_itemization = ''
 
