5b590aa53dd73256a6a4d6031954ce1e7b91dc75
Enrico <159620783+Enrico1391@users.noreply.github.com>
2024-04-01 10:01:56 +0200

    Update abruptly_goblins.py

--- cc_python_projects/abruptly_goblins.py
+++ cc_python_projects/abruptly_goblins.py
@@ -1,4 +1,4 @@
-# Games Store/Plan Abrutly Goblins game night (abruptly_goblins.py)bn
+# Games Store/Plan Abrutly Goblins game night (abruptly_goblins.py)
 # Create a system for organizing and tracking gamer’s availability.
 
 # Empty list to store the people who are attending game night
